366ca6e7629bb4ab451fddd79859a8b7c66d6e4d
Tyler Wolff <tylerwolff@gmail.com>
2019-08-15 22:06:42 -0400

    setup main app framework

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,30 @@
-<script>
-	export let name;
-</script>
-
 <style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		-webkit-font-smoothing: antialiased;
+	}
+
+	.main {
+		max-width: 400px;
+		margin: 1em auto;
+	}
+
+	header {
+		padding: 0.25em 0;
+		background:	#37474f;
+		box-shadow: 0 1px 3px rgba(0,0,0,0.5);
+	}
+
 	h1 {
-		color: purple;
+		color: #fff;
+		text-align: center;
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<header>
+	<h1>Svelte todos</h1>
+</header>
+<div class="main">
+	todos
+</div>

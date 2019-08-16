d8bc149457722830c7ad602671841d1a55ab6d43
Tyler Wolff <tylerwolff@gmail.com>
2019-08-15 23:39:24 -0400

    add toggle capability

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -12,18 +12,32 @@
 </script>
 
 <style>
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
   .todo {
     padding: 1.25em 0 1.25em 0.25em;
     border-bottom: 1px solid #b0bec5;
+    line-height: 1;
   }
 
   .todo:last-of-type {
     border-bottom: 0;
   }
 
+  .done {
+    text-decoration: line-through;
+  }
+
+  .checkbox {
+    margin: 0 0.25em 0 0;
+    vertical-align: top;
+  }
+
   button {
-    float: right;
-    height: 1em;
     line-height: 1em;
     margin: 0;
     color: #263238;
@@ -35,6 +49,9 @@
 </style>
 
 <li class="todo">
-  {todo.text}
+  <label>
+    <input class="checkbox" type="checkbox" bind:checked={todo.done} />
+    <span class:done={todo.done}>{todo.text}</span>
+  </label>
   <button type="button" title="delete item" on:click={deleteTodo}>&times;</button>
 </li>
